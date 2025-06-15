0345cf58956c0940df593e70d87542b30c548d7f
darshant <Darshanjyoti.Talukdar@fiery.com>
2025-06-15 23:41:21 +0530

    Simplifying crud.repository.js

--- src/repositories/crud.repository.js
+++ src/repositories/crud.repository.js
@@ -7,61 +7,35 @@ class CrudRepository{
     }
 
     async create(data){
-        try {
-            console.log("inside create of crudrepo", data)
             const response = await this.model.create(data);
             console.log(response)
             return response;
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
     }
 
     async destroy(data){
-        try {
             const response = await this.model.destroy({
                 where:{
                     id : data
                 }
             });
             return response;
-        } catch (error) {
-           // Logger.error("Error in Deleting the model");
-            throw error;
-        }
     }
 
     async get(data){
-        try {
             const response = await this.model.findByPk(data);
             return response;
-        } catch (error) {
-           // Logger.error("Error in Getting the data");
-            throw error;
-        }
     }
 
-    async get(){
-        try {
+    async getAll(){
             const response = await this.model.findAll();
             return response;
-        } catch (error) {
-          //  Logger.error("Error in Getting all the data");
-            throw error;
-        }
     }
 
     async update(id, data){
-        try {
             const response = await this.model.update(data,{
                 id: id
             });
             return response;
-        } catch (error) {
-           // Logger.error("Error in Updating the data");
-            throw error;
-        }
     }
 }
 
